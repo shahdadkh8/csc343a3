ff4ed94efc71d3af76b6287eef03144371aa4270
vanheesw <jana.vanheeswyk@mail.utoronto.ca>
2025-04-07 03:22:59 -0400

    completed q4 and q5, tested all queries on postgre, and created demo.txt

--- q4.sql
+++ q4.sql
@@ -1,2 +1,11 @@
 
 /* ---------- q4.sql by Shahdad and Jana ---------- */
+
+SELECT m.mid,
+       m.name,
+       COUNT(DISTINCT sp.session) AS session_count
+FROM member m
+JOIN session_participant sp ON m.mid = sp.participant
+GROUP BY m.mid, m.name
+ORDER BY session_count DESC
+LIMIT 1;
--- q5.sql
+++ q5.sql
@@ -1,2 +1,18 @@
 
 /* ---------- q5.sql by Shahdad and Jana ---------- */
+
+SELECT e.eid,
+       e.event_date,
+       e.location,
+       AVG(session_counts.participant_count) AS avg_participants
+FROM event e
+JOIN (
+    SELECT gs.event,
+           gs.sid,
+           COUNT(sp.participant) AS participant_count
+    FROM game_session gs
+    LEFT JOIN session_participant sp ON gs.sid = sp.session
+    GROUP BY gs.sid, gs.event
+) AS session_counts ON e.eid = session_counts.event
+GROUP BY e.eid, e.event_date, e.location
+ORDER BY e.eid;

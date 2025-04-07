e3afa58ec4c4b7a03c8aa2f05b0bdcee1146f280
vanheesw <jana.vanheeswyk@mail.utoronto.ca>
2025-04-07 16:30:00 -0400

    changes to q5 to include nulls after reading additional info on piazza

--- q5.sql
+++ q5.sql
@@ -1,15 +1,17 @@
 
 /* ---------- q5.sql by Shahdad and Jana ---------- */
 
-SELECT e.eid,
-       e.event_date,
-       e.location,
-       AVG(session_counts.participant_count) AS avg_participants
+SELECT
+    e.eid,
+    e.event_date,
+    e.location,
+    AVG(session_counts.participant_count) AS avg_participants
 FROM event e
-JOIN (
-    SELECT gs.event,
-           gs.sid,
-           COUNT(sp.participant) AS participant_count
+LEFT JOIN (
+    SELECT
+        gs.event,
+        gs.sid,
+        COUNT(sp.participant) AS participant_count
     FROM game_session gs
     LEFT JOIN session_participant sp ON gs.sid = sp.session
     GROUP BY gs.sid, gs.event
